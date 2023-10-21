8dc888a306319afa2434c77166912b77f742824a
noranuko13 <noranuko13@gmail.com>
2023-10-21 14:31:06 +0900

    :notes: [librarian] Bookbinding::Constant

---
+++ lib/bookbinding/constant.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Bookbinding
+  class Constant
+    DIST_DIR = 'dist'
+    JACKETS_DIR = 'jackets'
+    SHELVES_DIR = 'shelves'
+    UPGRADE_DIR = 'upgrade'
+    WORKBENCH_DIR = 'tmp/workbench'
+  end
+end
