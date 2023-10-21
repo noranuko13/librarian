3f7d7a9baa477db65591628170cfd1bceb624232
noranuko13 <noranuko13@gmail.com>
2023-10-21 18:31:53 +0900

    :notes: [Rake] bookbinding:pdf[repository/inspect,tech]

--- spec/lib/bookbinding/pdf_spec.rb
+++ spec/lib/bookbinding/pdf_spec.rb
@@ -3,11 +3,15 @@
 require 'rake_helper'
 
 RSpec.describe 'bookbinding:pdf', type: :task do
+  before do
+    allow($stdout).to receive(:write)
+  end
+
   context 'when valid params' do
     let(:task) { Rake.application['bookbinding:pdf'] }
 
     it 'Success' do
-      expect { task.invoke }.not_to raise_error
+      expect { task.invoke('repository/inspect', 'tech') }.not_to raise_error
     end
   end
 end
--- tasks/bookbinding.rake
+++ tasks/bookbinding.rake
@@ -2,7 +2,7 @@
 
 namespace :bookbinding do
   desc 'Bind PDF'
-  task :pdf do |_task, _args|
-    # Bind PDF
+  task :pdf, Bookbinding::Arguments::NAMES do |_, args|
+    Bookbinding::Command.new(args).execute
   end
 end
